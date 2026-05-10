b448461792cc84223508a07f2e725780ecf2cc54
Fabio <fabiomotamarques815@gmail.com>
2026-05-10 19:01:53 -0300

    Sinals e binding

--- src/app/hello/hello.ts
+++ src/app/hello/hello.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './hello.html',
   styleUrl: './hello.scss',
 })
-export class Hello {}
+export class Hello {
+  protected title = 'Meu primeiro componente Angular';
+  protected isDesabilitar = false;
+  protected onClick() {
+    this.isDesabilitar = !this.isDesabilitar;
+  }
+
+  protected count = signal(0);
+
+  incrementar() {
+    this.count.update(v => v += 1);
+  }
+  decrementar() {
+    this.count.update(v => v -= 1);
+  }
+  resetar() {
+    this.count.set(0);
+  }
+}
