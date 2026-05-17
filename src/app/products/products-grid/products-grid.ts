c8b6c3a228aec3d0c3f03844e8b793800dd80b94
Fabio <fabiomotamarques815@gmail.com>
2026-05-17 19:47:04 -0300

    usando for

--- src/app/products/products-grid/products-grid.ts
+++ src/app/products/products-grid/products-grid.ts
@@ -1,13 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { ProductsCard } from '../products-card/products-card';
 import { Products } from '../products';
+import { NgForOf } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-products-grid',
-  imports: [ProductsCard],
+  imports: [ProductsCard, NgForOf, MatIcon],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
 })
 export class ProductsGrid {
-  protected readonly products = signal<Products>
+  protected readonly products = signal<Products[]>([
+    {id: 1, name: 'Premium Weriless Headphones', description: 'descrição', price: 199.99, originalPrice: 249.90}
+  ])
 }
